149bbefe55d9356c0a4b92ee07650ce9bb0d79b1
LGDD-29 <1833165849@qq.com>
2026-01-20 20:10:21 +0800

    修改时钟源

--- Code/Core/Src/main.c
+++ Code/Core/Src/main.c
@@ -197,9 +197,9 @@ static void MX_TIM1_Init(void)
 
   /* USER CODE END TIM1_Init 1 */
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 99;
+  htim1.Init.Prescaler = 0;
   htim1.Init.CounterMode = TIM_COUNTERMODE_CENTERALIGNED1;
-  htim1.Init.Period = 1;
+  htim1.Init.Period = 2749;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim1.Init.RepetitionCounter = 0;
   htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
