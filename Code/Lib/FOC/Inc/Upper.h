c5f887338a683fba4b0d6572fa20485ddeb2aea1
LGDD-29 <1833165849@qq.com>
2026-01-21 18:26:27 +0800

    USART函数能用了，并且电脑能接收到

--- Code/Lib/FOC/Inc/Upper.h
+++ Code/Lib/FOC/Inc/Upper.h
@@ -1,10 +1,11 @@
 #ifndef __UPPER_H
 #define __UPPER_H
+#include "stm32f4xx_hal.h"
+#define CH_COUNT 1
 
-#define CH_COUNT 1//这里是需要发送的通道数量
 
 void Float_to_Byte(float f,unsigned char byte[]);
-void Float_send(float *data_array);
-void Send_array(unsigned char* byte, uint8_t Number);
+void Float_send(float *data_array,UART_HandleTypeDef* huart);
+void Send_array(unsigned char* byte, uint8_t Number, UART_HandleTypeDef* huart);
 
 #endif
--- Code/Lib/FOC/Src/Upper.c
+++ Code/Lib/FOC/Src/Upper.c
@@ -1,17 +1,13 @@
-#include "stm32f4xx_it.h"                // Device header
+#include "stm32f4xx_hal.h"
 #include <stdint.h>
 #include "Upper.h"
-//Ҫ����ʾ:
-//1.float��unsigned long������ͬ�����ݽṹ����
-//2.union������������ݴ������ͬ�������ռ�
 typedef union
 {
     float fdata;
     unsigned long ldata;
 } FloatLongType;
 
-//��������fת��Ϊ4���ֽ����ݴ����byte[4]��
-//����ָ��
+
 void Float_to_Byte(float f,unsigned char byte[])
 {
     FloatLongType fl;
@@ -21,21 +17,20 @@ void Float_to_Byte(float f,unsigned char byte[])
     byte[2]=(unsigned char)(fl.ldata>>16);
     byte[3]=(unsigned char)(fl.ldata>>24);
 }
-//JustfloatЭ�鷢��
-void Float_send(float *data_array)
+
+void Float_send(float *data_array,UART_HandleTypeDef* huart)
 {
-    //����Ϊ��������
-    unsigned char tail[4] = {0x00, 0x00, 0x80, 0x7f};//����֡β
-    //����ͨ�����������ݣ�CH_COUNTΪ�������ݵ�ͨ����
+
+    unsigned char tail[4] = {0x00, 0x00, 0x80, 0x7f};
     for (int i = 0; i < CH_COUNT; i++){
         unsigned char byte[4];
-        Float_to_Byte(data_array[i],byte); // ��ÿ��floatת��Ϊ4���ֽ�
-        Send_array(byte,4);           // ������4���ֽ�
+        Float_to_Byte(data_array[i],byte); 
+        Send_array(byte,4,huart);           
     }
-    Send_array(tail,4);//��������֡β
+    Send_array(tail,4,huart);//֡β
 }
 
-void Send_array(unsigned char* byte, uint8_t Number)
+void Send_array(unsigned char* byte, uint8_t Number, UART_HandleTypeDef* huart)
 {
     int32_t i;
 
@@ -45,10 +40,10 @@ void Send_array(unsigned char* byte, uint8_t Number)
 
     for(i = 0; i < Number; i++)
     {
-        while(USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET);
+       while(__HAL_UART_GET_FLAG(huart, UART_FLAG_TXE) == RESET);
 
-        USART_SendData(USART2, byte[i]);
+       HAL_UART_Transmit(huart, &byte[i], 1, HAL_MAX_DELAY);
     }
 
-    while(USART_GetFlagStatus(USART2, USART_FLAG_TC) == RESET);
+    while(__HAL_UART_GET_FLAG(huart, UART_FLAG_TC) == RESET);
 }
