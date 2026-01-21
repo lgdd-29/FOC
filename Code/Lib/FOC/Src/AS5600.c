996cba2d2952172e6c5b861e6bac20774858e0b2
LGDD-29 <1833165849@qq.com>
2026-01-21 22:52:17 +0800

    波形更丝滑了

--- Code/Lib/FOC/Src/AS5600.c
+++ Code/Lib/FOC/Src/AS5600.c
@@ -10,11 +10,10 @@ void AS5600_Init(AS5600_Driver_t* self)
     self->i2c->MyI2C_Init(self->i2c);
 }
 
-float AS5600_READ(AS5600_Driver_t* self)
+uint8_t AS5600_READ(AS5600_Driver_t* self,uint8_t reg)
 {
-    uint8_t Data1,Data2;
+    uint8_t Data;
     uint8_t ack;
-    float Angle;
     self->i2c->MyI2C_Start(self->i2c);
     self->i2c->MyI2C_Send_Byte(self->i2c,(self->AS5600_ADDRESS<<1)|self->WRITE_OP);
     self->i2c->MyI2C_ReceiveAck(self->i2c,&ack);
@@ -23,7 +22,7 @@ float AS5600_READ(AS5600_Driver_t* self)
         self->i2c->MyI2C_Stop(self->i2c);
         return -1;
     }
-    self->i2c->MyI2C_Send_Byte(self->i2c,self->ANGLE_1_OP);
+    self->i2c->MyI2C_Send_Byte(self->i2c,reg);
     self->i2c->MyI2C_ReceiveAck(self->i2c,&ack);
     if(ack!=0)
     {
@@ -38,13 +37,20 @@ float AS5600_READ(AS5600_Driver_t* self)
         self->i2c->MyI2C_Stop(self->i2c);
         return -1;
     }
-    self->i2c->MyI2C_Receive_Byte(self->i2c,&Data1);
+    self->i2c->MyI2C_Receive_Byte(self->i2c,&Data);
     self->i2c->MyI2C_SendAck(self->i2c,1);
-    self->i2c->MyI2C_Receive_Byte(self->i2c,&Data2);
-    self->i2c->MyI2C_SendAck(self->i2c,0);
     self->i2c->MyI2C_Stop(self->i2c);
-    Angle=((Data1<<8)|Data2);
-    return (foc_float_t)Angle*360.0f/4096.0f;
+    return Data;
+}
+
+uint16_t AS5600_GetRaw(AS5600_Driver_t* self)
+{
+    uint8_t Angle_H,Angle_L;
+    uint16_t RawAngle;
+    Angle_H=AS5600_READ(self,self->ANGLE_1_OP);
+    Angle_L=AS5600_READ(self,self->ANGLE_2_OP);
+    RawAngle=(((uint16_t)Angle_H&0x0F)<<8)|Angle_L;
+    return RawAngle;
 }
 
 void AS5600_Calibrarion(AS5600_Driver_t* self)
@@ -52,7 +58,7 @@ void AS5600_Calibrarion(AS5600_Driver_t* self)
     //TODO 校准函数
     for(uint8_t i=0;i<10;i++)
     {
-        self->Angle_error+=AS5600_READ(self);
+        self->Angle_error+=AS5600_GetRaw(self);
     }
     self->Angle_error/=10.0f;
 
@@ -75,7 +81,7 @@ AS5600_Driver_t* AS5600_Create(MyI2C_Driver_t* i2c,uint8_t READ_OP,uint8_t WRITE
 
         //函数映射
         driver->Init = AS5600_Init;
-        driver->GetAngle = AS5600_READ;
+        driver->GetAngle = AS5600_GetRaw;
         driver->AS5600_Calibrarion = AS5600_Calibrarion;
     }
     return driver;
