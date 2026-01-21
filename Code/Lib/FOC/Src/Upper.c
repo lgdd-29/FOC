f39be6287246029e5ed9b8ab57f23ba8d801c88a
LGDD-29 <1833165849@qq.com>
2026-01-21 16:47:49 +0800

    修改头文件

--- Code/Lib/FOC/Src/Upper.c
+++ Code/Lib/FOC/Src/Upper.c
@@ -1,16 +1,17 @@
-#include "stm32f10x.h"                  // Device header
+#include "stm32f4xx_it.h"                // Device header
+#include <stdint.h>
 #include "Upper.h"
-//要点提示:
-//1.float和unsigned long具有相同的数据结构长度
-//2.union据类型里的数据存放在相同的物理空间
+//要锟斤拷锟斤拷示:
+//1.float锟斤拷unsigned long锟斤拷锟斤拷锟斤拷同锟斤拷锟斤拷锟捷结构锟斤拷锟斤拷
+//2.union锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷荽锟斤拷锟斤拷锟斤拷同锟斤拷锟斤拷锟斤拷锟秸硷拷
 typedef union
 {
     float fdata;
     unsigned long ldata;
 } FloatLongType;
 
-//将浮点数f转化为4个字节数据存放在byte[4]中
-//传递指针
+//锟斤拷锟斤拷锟斤拷锟斤拷f转锟斤拷为4锟斤拷锟街斤拷锟斤拷锟捷达拷锟斤拷锟絙yte[4]锟斤拷
+//锟斤拷锟斤拷指锟斤拷
 void Float_to_Byte(float f,unsigned char byte[])
 {
     FloatLongType fl;
@@ -20,23 +21,23 @@ void Float_to_Byte(float f,unsigned char byte[])
     byte[2]=(unsigned char)(fl.ldata>>16);
     byte[3]=(unsigned char)(fl.ldata>>24);
 }
-//Justfloat协议发送
+//Justfloat协锟介发锟斤拷
 void Float_send(float *data_array)
 {
-    //参数为浮点数组
-    unsigned char tail[4] = {0x00, 0x00, 0x80, 0x7f};//定义帧尾
-    //按照通道数发送数据，CH_COUNT为发送数据的通道数
+    //锟斤拷锟斤拷为锟斤拷锟斤拷锟斤拷锟斤拷
+    unsigned char tail[4] = {0x00, 0x00, 0x80, 0x7f};//锟斤拷锟斤拷帧尾
+    //锟斤拷锟斤拷通锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟捷ｏ拷CH_COUNT为锟斤拷锟斤拷锟斤拷锟捷碉拷通锟斤拷锟斤拷
     for (int i = 0; i < CH_COUNT; i++){
         unsigned char byte[4];
-        Float_to_Byte(data_array[i],byte); // 将每个float转换为4个字节
-        Send_array(byte,4);           // 发送这4个字节
+        Float_to_Byte(data_array[i],byte); // 锟斤拷每锟斤拷float转锟斤拷为4锟斤拷锟街斤拷
+        Send_array(byte,4);           // 锟斤拷锟斤拷锟斤拷4锟斤拷锟街斤拷
     }
-    Send_array(tail,4);//发送数据帧尾
+    Send_array(tail,4);//锟斤拷锟斤拷锟斤拷锟斤拷帧尾
 }
 
 void Send_array(unsigned char* byte, uint8_t Number)
 {
-    uint8_t i;
+    int32_t i;
 
     if(byte == 0 || Number == 0){
         return;
